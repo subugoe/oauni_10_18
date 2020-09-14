3501b4268cbea03c3d877b66b408f714a4ed459c
Ahobert <anne@hobert.eu>
2020-09-14 17:27:16 +0200

    added category selection for boxplot, closes #3

--- data-raw/oa_shares_inst_sec_boxplot.R
+++ data-raw/oa_shares_inst_sec_boxplot.R
@@ -1,6 +1,7 @@
 ## code to prepare boxplot dataset goes here
 library(dplyr)
 library(readr)
+library(forcats)
 
 pubs_cat <-
   readr::read_csv("data-raw/pubs_cat.csv", col_types = "dccdcclc")
@@ -66,11 +67,42 @@ all_inst_sec <-  pubs_cat %>%
   filter(!INST_NAME %in% inst_to_drop) %>%
   group_by(PUBYEAR, INST_NAME, sector, sec_abbr) %>%
   summarise(articles = n_distinct(PK_ITEMS))
-oa_shares_inst_sec_boxplot <- all_inst_sec %>% 
-  left_join(oa_inst_sec)  %>%
+cat_inst_sec <- pubs_cat %>% 
+  filter(!INST_NAME %in% inst_to_drop) %>%
+  group_by(PUBYEAR, INST_NAME, sector, sec_abbr, oa_category) %>% 
+  summarise(n_cat = n_distinct(PK_ITEMS)) %>%
+  ungroup()
+host_inst_sec <- pubs_cat %>% 
+  filter(!INST_NAME %in% inst_to_drop) %>%
+  mutate(oa_host = fct_collapse(
+    oa_category,
+    journal = c("full_oa_journal", "other_oa_journal"),
+    repository = c(
+      "opendoar_inst",
+      "opendoar_subject",
+      "opendoar_other",
+      "other_repo"
+    ),
+    not_oa = c("not_oa")
+  )) %>%
+  group_by(PUBYEAR, INST_NAME, sector, sec_abbr, oa_host) %>% 
+  mutate(n_host = n_distinct(PK_ITEMS)) %>%
   ungroup() %>% 
-  mutate(oa_articles = ifelse(is.na(oa_articles), 0, oa_articles)) %>% 
+  group_by(PUBYEAR, INST_NAME, sector, sec_abbr, oa_category, oa_host, n_host) %>% 
+  summarise() %>% 
+  ungroup()
+oa_shares_inst_sec_boxplot <- all_inst_sec %>% 
+  full_join(pubs_cat %>% distinct(oa_category), by = character()) %>% 
+  left_join(cat_inst_sec) %>%
+  left_join(host_inst_sec) %>% 
+  left_join(oa_inst_sec)  %>%
+  ungroup() %>%
+  mutate(oa_articles = ifelse(is.na(oa_articles), 0, oa_articles),
+         n_cat = ifelse(is.na(n_cat), 0, n_cat),
+         n_host = ifelse(is.na(n_host), 0, n_host)) %>% 
   mutate(prop = oa_articles / articles) %>%
+  mutate(prop_cat = n_cat / articles) %>% 
+  mutate(prop_host = n_host / articles) %>% 
   mutate(sector_cat = case_when(
     sec_abbr == "UNI" ~ "Universities",
     sec_abbr %in% c("MPS", "HGF") ~ "Research-oriented",

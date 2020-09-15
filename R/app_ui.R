127f325d676de2c9e16d2609ab81ce796b5533c0
Maximilian Held <info@maxheld.de>
2020-09-16 01:18:03 +0200

    comment out favicon maybe for #34

--- R/app_ui.R
+++ R/app_ui.R
@@ -75,11 +75,10 @@ app_ui <- function(request) {
 #' @noRd
 golem_add_external_resources <- function() {
   add_resource_path('www', app_sys('app/www'))
-  
-  tags$head(favicon(),
-            bundle_resources(path = app_sys('app/www'),
+
+  tags$head(bundle_resources(path = app_sys('app/www'),
                              app_title = 'oadash')
             # Add here other external resources
-            # for example, you can add shinyalert::useShinyalert() 
+            # for example, you can add shinyalert::useShinyalert()
             )
 }
